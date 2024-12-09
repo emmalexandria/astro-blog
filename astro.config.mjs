597daf44ee0f03c820169fa2eb732c638a61e7a0
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-12-09 17:35:46 +0200

    Finish contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,24 @@ import rehypeSlug from 'rehype-slug';
 import svelte from '@astrojs/svelte';
 
 import compress from 'astro-compress';
-
 import purgecss from 'astro-purgecss';
-
 import compressor from 'astro-compressor';
+
+import { latte, macchiato } from '@catppuccin/vscode';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.emmalexandria.dev',
   markdown: {
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { properties: { class: 'heading-link' }, behavior: 'wrap' }]],
+    shikiConfig: {
+      themes: {
+        light: latte,
+        dark: macchiato
+      }
+    }
   },
-  integrations: [mdx(), sitemap(), tailwind(), svelte(), compress({ Image: false }), purgecss(), compressor()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), compress({ Image: false }), purgecss(), compressor(), icon()]
 });
